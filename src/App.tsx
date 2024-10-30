ae3d134cac57a72dab5e60871fad72c8e759b04d
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2024-10-30 20:04:30 +0300

    feat: add completed project (#1)
    * feat: create vite react app
    
    * feat: add binance logo
    
    * refactor: rename title
    
    * refactor: delete default vite app code
    
    * refactor: delete react logo
    
    * refactor: change style for logo and add recharts library
    
    * feat: add api get binance data
    
    * feat: get data from useEffect and save to state
    
    * feat: parse data for chart
    
    * feat: add victory diagram
    
    * refactor: get 24 hours for 1h data
    
    * refactor: correct diagram style
    
    * refactor: delete index css
    
    * feat: add eslint and prettier to project
    
    * feat: add Diagram component
    
    * refactor: format code with lint and prettier
    
    * feat: add script for deploy on my github
    
    * refactor: change folder for deploy build project
    
    * refactor: change path for build project with my github

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import viteLogo from '/binance-logo.svg';
+import Diagram from './components/Diagram';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <img src={viteLogo} className="logo" alt="Binance" />
+      <h2>BTC/USDT</h2>
+      <Diagram />
+    </>
+  );
+};
+
+export default App;
