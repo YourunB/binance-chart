70601995a00de8a9ba817307f1487ba81dbd8ce9
yourunb <bxyura@gmail.com>
2024-10-30 16:53:33 +0300

    feat: add api get binance data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import viteLogo from '/binance-logo.svg';
+import { useEffect } from 'react';
+import { getBinanceDataApi } from './api/getBinanceDataApi';
 
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    getBinanceDataApi();
+  }, []);
+
   return (
     <>
       <img src={viteLogo} className="logo" alt="Binance" />
---
+++ src/api/getBinanceDataApi.ts
@@ -0,0 +1,14 @@
+const url = 'https://api.binance.com/';
+
+export const getBinanceDataApi = async () => {
+  try {
+    const response = await fetch(
+      `${url}api/v3/klines?symbol=BTCUSDT&interval=1h&limit=1000`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
